f05555eecee5b2604d054c1439669282378767fd
bayu <bpamuji93@gmail.com>
2019-01-29 14:02:13 +0700

    add logic

--- app/src/main/java/id/bukusaku/dillo/MainActivity.kt
+++ app/src/main/java/id/bukusaku/dillo/MainActivity.kt
@@ -2,11 +2,27 @@ package id.bukusaku.dillo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_count.setOnClickListener { countScore() }
+        btn_toast.setOnClickListener { showToast() }
+    }
+
+    private fun showToast() {
+        Toast.makeText(this,"Your Score is $score",Toast.LENGTH_LONG).show()
+    }
+
+    private fun countScore() : Int {
+        score = tv_score.text.toString().toInt()
+        score += 1
+        tv_score.text = score.toString()
+        return score
     }
 }
